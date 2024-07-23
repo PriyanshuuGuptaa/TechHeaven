de69ba9b0cb2e09d81cb25e2914ffeb78339b79d
PriyanshuuGuptaa <priyanshuuguptaa1470@gmail.com>
2024-07-23 12:54:04 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
 //configure env
 dotenv.config();
 
@@ -25,23 +27,28 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
 
+// Use import.meta.url to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the "build" directory
+app.use(express.static(path.join(__dirname, "build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/products", productRoute);
-// rest api
 
-
+// Handle all other routes by serving the index.html file
 app.use("*", function (req, res) {
     res.sendFile(path.join(__dirname, "build/index.html"));
 })
 
 //PORT
-// run listen
-app.listen(process.env.BACKEND_PORT, () => {
-    console.log(`server running on ${process.env.BACKEND_PORT}`.bgCyan.white);
+const PORT = process.env.BACKEND_PORT || 5000;
 
-})
+// run listen
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`.bgCyan.white);
+});
